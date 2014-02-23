ddfa066d949e7552005d61e52a29e6aa10ae8ab0
Jason <1lanncontact@gmail.com>
2014-02-23 19:12:09 +0800

    Update

--- scripts/apis/fs.js
+++ scripts/apis/fs.js
@@ -9,46 +9,378 @@
 var fsAPI = {};
 var filer = null;
 
-serialize = function(arr) {
-	var construct = "{"
+var filesystem = {};
+var callID = 0;
+
+var readOnly = [
+	"/*/rom",
+];
+
+
+
+//  ----------------  Filesystem API  ----------------  //
+
+
+filesystem.setup = function(callback) {
+	filer = new Filer();
+	filer.init({"persistent": true, "size": 4 * 1024 * 1024}, function(fs) {
+		callback(null);
+	}, function(err) {
+		console.log("Failed to load filesystem!");
+		console.log("Unable to start WebCC!");
+		console.log(err);
+
+		callback(err);
+	});
+}
+
+
+filesystem.serializeTable = function(arr) {
+	var construct = "{";
 	for (var index in arr) {
-		var name = arr[index].replace("\"","\\\"");
-		var correctIndex = parseInt(index)+1;
-		construct = construct+"["+correctIndex+"]=\""+name+"\","
+		var name = arr[index].replace("\"", "\\\"");
+		var correctIndex = parseInt(index) + 1;
+		construct = construct + "[" + correctIndex.toString() + "]=\"" + name + "\",";
 	}
-	construct = construct+"}"
+	construct = construct + "}";
+
 	return construct;
 }
 
-onFSError = function(err) {
-	console.log(err);
+
+filesystem.resolve = function(path) {
+	path = filesystem.sanitise(path);
+
+	// Replace simple resolutions
+	path = path.replace(/(\/(\.\/)+)|(\/\.$)/g, "/").replace(/\/{2,}/g, "/");
+
+	// Replace ../
+	var leadingParents = path.substring(1).match(/^(\.\.\/)+/) || '';
+	if (leadingParents) {
+		leadingParents = leadingParents[0];
+	}
+
+	while (true) {
+		var parent = path.indexOf("/..");
+		if (parent == -1) {
+			break;
+		} else if (parent == 0) {
+			path = path.substring(3);
+			continue;
+		}
+
+		var pos = path.substring(0, parent).lastIndexOf("/");
+		if (pos == -1) {
+			pos = parent;
+		}
+		path = path.substring(0, pos) + path.substring(parent + 3);
+	}
+
+	path = leadingParents + path.substring(1);
+	return filesystem.sanitise("/" + computer.id + "/" + path);
 }
 
-setupFSAPI = function() {
-	filer = new Filer();
-	filer.init({"persistent": true, "size": 4 * 1024 * 1024}, function(fs) {
-		console.log(fs);
-	}, onFSError);
-}
 
-
-resolve = function(path) {
+filesystem.sanitise = function(path) {
+	path = path.replace("\\", "/");
 	if (path.substring(0, 1) != "/") {
 		path = "/" + path;
 	}
-	path = "/" + computer.id + path;
+
+	if (path.substring(path.length - 1) == "/") {
+		path = path.substring(0, path.length - 1);
+	}
+
+	if (path.length == 0) {
+		path = "/";
+	}
+
 	return path;
 }
 
 
-exists = function(path, callback) {
+filesystem.exists = function(path, callback) {
+	path = filesystem.sanitise(path);
 
+	var dir = path.substring(0, path.lastIndexOf("/"));
+	filer.ls(dir, function(items) {
+		for (var i in items) {
+			var item = items[i];
+			if (item.fullPath == path) {
+				callback(true);
+				return;
+			}
+		}
+
+		callback(false);
+	}, function(err) {
+		callback(false);
+	});
 }
 
 
-fsAPI.list = function(L) {
-	var path = resolve(C.luaL_checkstring(L, 1));
+filesystem.isDir = function(path, callback) {
+	path = filesystem.sanitise(path);
+	if (path == "/") {
+		callback(true);
+		return;
+	}
 
+	var dir = path.substring(0, path.lastIndexOf("/"));
+	filer.ls(dir, function(items) {
+		for (var i in items) {
+			var item = items[i];
+			if (item.fullPath == path && item.isDirectory) {
+				callback(true);
+				return;
+			}
+		}
+
+		callback(false);
+	}, function(err) {
+		callback(false);
+	});
+}
+
+
+filesystem.isReadOnly = function(path) {
+	path = filesystem.sanitise(path).substring(1);
+	var parts = path.split("/");
+	
+	for (var i in readOnly) {
+		var readOnlyPath = filesystem.sanitise(readOnly[i]).substring(1).split("/");
+		if (parts.length >= readOnlyPath.length) {
+			var isReadOnly = true;
+			for (var ii in readOnlyPath) {
+				if (readOnlyPath[ii] == "*") {
+					continue;
+				}
+
+				if (readOnlyPath[ii] != parts[ii]) {
+					isReadOnly = false;
+					break;
+				}
+			}
+
+			if (isReadOnly) {
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+
+filesystem.list = function(path, callback) {
+	path = filesystem.sanitise(path);
+	
+	filer.ls(path, function(items) {
+		var files = [];
+		for (var i in items) {
+			var item = items[i];
+			files.push(item.name);
+		}
+
+		callback(files, null);
+	}, function(err) {
+		console.log(err);
+		if (err.code == err.NOT_FOUND_ERR) {
+			callback([], null);
+		} else {
+			callback(null, err);
+		}
+	});
+}
+
+
+filesystem.move = function(from, to, callback) {
+	from = filesystem.sanitise(from);
+	to = filesystem.sanitise(to);
+	
+	filesystem.isDir(to, function(is) {
+		var toDir = to.substring(0, to.lastIndexOf("/"));
+		var toName = to.substring(to.lastIndexOf("/") + 1);
+		if (is) {
+			toDir = to;
+			toName = from.substring(from.lastIndexOf("/") + 1);
+		}
+
+		filer.mv(from, toDir, toName, function(file) {
+			callback(null);
+		}, function(err) {
+			if (err.code == err.NOT_FOUND_ERR) {
+				callback(null);
+			} else {
+				callback(err);
+			}
+		});
+	});
+}
+
+
+filesystem.copy = function(from, to, callback) {
+	from = filesystem.sanitise(from);
+	to = filesystem.sanitise(to);
+	
+	filesystem.isDir(to, function(is) {
+		var toDir = to.substring(0, to.lastIndexOf("/"));
+		var toName = to.substring(to.lastIndexOf("/") + 1);
+		if (is) {
+			toDir = to;
+			toName = from.substring(from.lastIndexOf("/") + 1);
+		}
+
+		filer.cp(from, toDir, toName, function(file) {
+			callback(null);
+		}, function(err) {
+			if (err.code == err.NOT_FOUND_ERR) {
+				callback(null);
+			} else {
+				callback(err);
+			}
+		});
+	});
+}
+
+
+filesystem.delete = function(path, callback) {
+	path = filesystem.sanitise(path);
+	
+	filer.rm(path, function() {
+		callback(null);
+	}, function(err) {
+		if (err.code == err.NOT_FOUND_ERR) {
+			callback(null);
+		} else {
+			callback(err);
+		}
+	});
+}
+
+
+filesystem.read = function(path, callback) {
+	path = filesystem.sanitise(path);
+	
+	filesystem.isDir(path, function(is) {
+		if (is) {
+			callback(null, "dir");
+			return;
+		}
+
+		filer.open(path, function(file) {
+			var reader = new FileReader();
+			reader.onloadend = function(e) {
+				callback(e.target.result, null);
+			}
+
+			reader.onerror = function(err) {
+				callback(null, err);
+			}
+
+			reader.onabort = function(err) {
+				callback(null, err);
+			}
+
+			reader.readAsText(file);
+		}, function(err) {
+			callback(null, err);
+		});
+	});
+}
+
+
+filesystem.write = function(path, contents, append, callback) {
+	path = filesystem.sanitise(path);
+	
+	filesystem.isDir(path, function(is) {
+		if (is) {
+			callback("dir");
+			return;
+		}
+
+		var complete = function() {
+			filer.write(path, {"data": contents, "append": append}, function(entry, writer) {
+				callback(null);
+			}, function(err) {
+				callback(err);
+			});
+		}
+
+		var dir = path.substring(0, path.lastIndexOf("/"));
+		if (dir.length > 0) {
+			filesystem.makeDir(dir, function(err) {
+				if (err) {
+					callback(err);
+				} else {
+					complete();
+				}
+			});
+		} else {
+			complete();
+		}
+	});
+}
+
+
+filesystem.makeDir = function(path, callback) {
+	path = filesystem.sanitise(path) + "/";
+	if (path == "/") {
+		callback(null);
+		return;
+	}
+
+	var dir = path.substring(1);
+	var currentDir = "/" + dir.substring(0, dir.indexOf("/"));
+	dir = dir.substring(dir.indexOf("/") + 1);
+
+	var make = function() {
+		var next = function() {
+			if (dir.indexOf("/") == -1) {
+				callback(null);
+			} else {
+				currentDir = currentDir + "/" + dir.substring(0, dir.indexOf("/"));
+				dir = dir.substring(dir.indexOf("/") + 1);
+				make();
+			}
+		}
+
+		filer.mkdir(currentDir, true, next, function(err) {
+			if (err.code != err.INVALID_MODIFICATION_ERR) {
+				callback(err);
+			} else {
+				next();
+			}
+		});
+	}
+
+	make();
+}
+
+
+
+//  ----------------  Lua Wrappers  ----------------  //
+
+
+fsAPI.list = function(L) {
+	var path = filesystem.resolve(C.luaL_checkstring(L, 1));
+
+	var currentCallID = callID;
+	C.lua_pushnumber(L, currentCallID);
+	callID += 1;
+
+	filesystem.list(path, function(files, err) {
+		if (err) {
+			console.log("fs_list error", err);
+			return;
+		}
+
+		console.log("list", files);
+		computer.eventStack.push(["fs_list", currentCallID, files]);
+		resumeThread();
+	});
+
+	return 1;
 }
 
 
@@ -59,50 +391,197 @@ fsAPI.getSize = function(L) {
 
 
 fsAPI.exists = function(L) {
+	var path = filesystem.resolve(C.luaL_checkstring(L, 1));
 
+	var currentCallID = callID;
+	C.lua_pushnumber(L, currentCallID);
+	callID += 1;
+
+	filesystem.exists(path, function(exists) {
+		computer.eventStack.push(["fs_exists", currentCallID, exists]);
+		resumeThread();
+	});
+
+	return 1;
 }
 
 
 fsAPI.isDir = function(L) {
+	var path = filesystem.resolve(C.luaL_checkstring(L, 1));
 
+	var currentCallID = callID;
+	C.lua_pushnumber(L, currentCallID);
+	callID += 1;
+
+	filesystem.isDir(path, function(is) {
+		computer.eventStack.push(["fs_isDir", currentCallID, is]);
+		resumeThread();
+	});
+
+	return 1;
 }
 
 
 fsAPI.isReadOnly = function(L) {
+	var path = filesystem.resolve(C.luaL_checkstring(L, 1));
+	var isReadOnly = filesystem.isReadOnly(path);
+	C.lua_pushnumber(L, isReadOnly ? 1 : 0);
 
+	return 1;
 }
 
 
 fsAPI.makeDir = function(L) {
+	var path = filesystem.resolve(C.luaL_checkstring(L, 1));
 
+	var currentCallID = callID;
+	C.lua_pushnumber(L, currentCallID);
+	callID += 1;
+
+	filesystem.makeDir(path, function(err) {
+		if (err) {
+			console.log("fs_makeDir error", err);
+		}
+
+		computer.eventStack.push(["fs_makeDir", currentCallID]);
+		resumeThread();
+	});
+
+	return 1;
 }
 
 
 fsAPI.move = function(L) {
+	var from = filesystem.resolve(C.luaL_checkstring(L, 1));
+	var to = filesystem.resolve(C.luaL_checkstring(L, 2));
 
+	var currentCallID = callID;
+	C.lua_pushnumber(L, currentCallID);
+	callID += 1;
+
+	filesystem.move(from, to, function(err) {
+		if (err) {
+			console.log("fs_move error", err);
+		}
+
+		computer.eventStack.push(["fs_move", currentCallID]);
+		resumeThread();
+	});
+	
+	return 1;
 }
 
 
 fsAPI.copy = function(L) {
+	var from = filesystem.resolve(C.luaL_checkstring(L, 1));
+	var to = filesystem.resolve(C.luaL_checkstring(L, 2));
 
+	var currentCallID = callID;
+	C.lua_pushnumber(L, currentCallID);
+	callID += 1;
+
+	filesystem.copy(from, to, function(err) {
+		if (err) {
+			console.log("fs_copy error", err);
+		}
+
+		computer.eventStack.push(["fs_copy", currentCallID]);
+		resumeThread();
+	});
+	
+	return 1;
 }
 
 
 fsAPI.delete = function(L) {
+	var path = filesystem.resolve(C.luaL_checkstring(L, 1));
 
+	var currentCallID = callID;
+	C.lua_pushnumber(L, currentCallID);
+	callID += 1;
+
+	filesystem.delete(path, function(err) {
+		if (err) {
+			console.log("fs_delete error", err);
+		}
+
+		computer.eventStack.push(["fs_delete", currentCallID]);
+		resumeThread();
+	});
+
+	return 1;
 }
 
 
-fsAPI.open = function(L) {
+fsAPI.write = function(L) {
+	var path = filesystem.resolve(C.luaL_checkstring(L, 1));
+	var contents = C.luaL_checkstring(L, 2);
 
+	var currentCallID = callID;
+	C.lua_pushnumber(L, currentCallID);
+	callID += 1;
+
+	filesystem.write(path, contents, false, function(err) {
+		if (err) {
+			console.log("fs_write error", err);
+		}
+
+		computer.eventStack.push(["fs_write", currentCallID]);
+		resumeThread();
+	});
+
+	return 1;
+}
+
+
+fsAPI.append = function(L) {
+	var path = filesystem.resolve(C.luaL_checkstring(L, 1));
+	var contents = C.luaL_checkstring(L, 2);
+
+	var currentCallID = callID;
+	C.lua_pushnumber(L, currentCallID);
+	callID += 1;
+
+	filesystem.write(path, contents, true, function(err) {
+		if (err) {
+			console.log("fs_write error", err);
+			return;
+		}
+
+		computer.eventStack.push(["fs_append", currentCallID]);
+		resumeThread();
+	});
+	
+	return 1;
+}
+
+
+fsAPI.read = function(L) {
+	var path = filesystem.resolve(C.luaL_checkstring(L, 1));
+
+	var currentCallID = callID;
+	C.lua_pushnumber(L, currentCallID);
+	callID += 1;
+
+	filesystem.read(path, function(contents, err) {
+		if (err) {
+			console.log("fs_read error", err);
+			return;
+		}
+
+		computer.eventStack.push(["fs_read", currentCallID, contents]);
+		resumeThread();
+	});
+	
+	return 1;
 }
 
 
 fsAPI.getDrive = function(L) {
-
+	return 0;
 }
 
 
 fsAPI.getFreeSpace = function(L) {
-
+	return 0;
 }
--- scripts/computercraft.js
+++ scripts/computercraft.js
@@ -112,122 +112,31 @@ var loadAPIs = function() {
 //  ----------------  Main  ----------------  //
 
 
-var code = "\
-xpcall = function( _fn, _fnErrorHandler ) \n\
-	local typeT = type( _fn ) \n\
-	assert( typeT == 'function', 'bad argument #1 to xpcall (function expected, got '..typeT..')' ) \n\
-	local co = coroutine.create( _fn ) \n\
-	local tResults = { coroutine.resume( co ) } \n\
-	while coroutine.status( co ) ~= 'dead' do \n\
-		tResults = { coroutine.resume( co, coroutine.yield() ) } \n\
-	end \n\
-	if tResults[1] == true then \n\
-		return true, unpack( tResults, 2 ) \n\
-	else \n\
-		return false, _fnErrorHandler( tResults[2] ) \n\
-	end \n\
-end \n\
-pcall = function( _fn, ... ) \n\
-	local typeT = type( _fn ) \n\
-	assert( typeT == 'function', 'bad argument #1 to pcall (function expected, got '..typeT..')' ) \n\
-	local tArgs = { ... } \n\
-	return xpcall(  \n\
-		function() \n\
-			return _fn( unpack( tArgs ) ) \n\
-		end, \n\
-		function( _error ) \n\
-			return _error \n\
-		end \n\
-	) \n\
-end \n\
-local function newLine() \n\
-local wid, hi = term.getSize() \n\
-local x, y = term.getCursorPos() \n\
-if y == hi then \n\
-  term.scroll(1) \n\
-  term.setCursorPos(1, y) \n\
-else \n\
-  term.setCursorPos(1, y+1) \n\
-end \n\
-end \n\
-local nativeShutdown = os.shutdown \n\
-function os.shutdown() \n\
-nativeShutdown() \n\
-while true do \n\
-  coroutine.yield() \n\
-end \n\
-end \n\
-local nativeReboot = os.reboot \n\
-function os.reboot() \n\
-nativeReboot() \n\
-while true do \n\
-  coroutine.yield() \n\
-end \n\
-end \n\
-local function reader() \n\
-local data = '' \n\
-local visibleData = '' \n\
-local startX, startY = term.getCursorPos() \n\
-local wid, hi = term.getSize() \n\
-while true do \n\
-  term.setCursorBlink(true) \n\
-  local e, p1 = coroutine.yield() \n\
-  if e == 'key' and p1 == 14 then \n\
-   data = data:sub(1, -2) \n\
-  elseif e == 'key' and p1 == 28 then \n\
-   newLine() \n\
-   return data \n\
-  elseif e == 'char' then \n\
-   data = data .. p1 \n\
-  end \n\
-  term.setCursorPos(startX, startY) \n\
-  if #data+startX+1 > wid then \n\
-   visibleData = data:sub(-1*(wid-startX-1)) \n\
-  else \n\
-   visibleData = data \n\
-  end \n\
-  term.write(visibleData .. ' ') \n\
-  local curX, curY = term.getCursorPos() \n\
-  term.setCursorPos(curX-1, curY) \n\
-end \n\
-end \n\
-while true do \n\
-term.setTextColor(1) \n\
-term.setBackgroundColor(32768) \n\
-term.write('lua> ') \n\
-local toRun, cError = loadstring(reader(), 'error') \n\
-if toRun then \n\
-  setfenv(toRun, getfenv(1)) \n\
-  local results = {pcall(toRun)} \n\
-  term.setBackgroundColor(32768) \n\
-  if results[1] then \n\
-    table.remove(results,1) \n\
-    term.write('-- Return values --') \n\
-    for k,v in pairs(results) do \n\
-      newLine() \n\
-      term.write(tostring(v)) \n\
-    end \n\
-  else \n\
-   if term.isColor() then \n\
-    term.setTextColor(16384) \n\
-   end \n\
-   term.write(results[2]) \n\
-  end \n\
-else \n\
-  if term.isColor() then \n\
-   term.setTextColor(16384) \n\
-  end \n\
-  term.write(cError) \n\
-end \n\
-newLine() \n\
-end \n\
-";
-
-
 callLua = function(data) {
 	C.luaL_dostring(L, data);
 }
 
+threadHang = function() {
+	thread.alive = false;
+	for (var i = 1; i <= term.height; i++) {
+		render.text(1, i, " ".repeat(term.width), "#000000", "#0000aa");
+	}
+
+	var errorOne = "The Lua thread had hung, and has been";
+	var errorTwo = "been terminated. This is equivelant to";
+	var errorThree = "\"Too long without yielding.\"";
+	var errorFour = "Please reboot the computer";
+	var startOne = Math.round((term.width / 2) - ((errorOne.length) / 2));
+	var startTwo = Math.round((term.width / 2) - ((errorTwo.length) / 2));
+	var startThree = Math.round((term.width / 2) - ((errorThree.length) / 2));
+	var startFour = Math.round((term.width / 2) - ((errorFour.length) / 2));
+	term.cursorBlink = false;
+	render.text(16, 7, "FATAL : THREAD HANG", "#0000aa", "#ffffff");
+	render.text(startOne, 9, errorOne, "#ffffff", "#0000aa");
+	render.text(startTwo, 10, errorTwo, "#ffffff", "#0000aa");
+	render.text(startThree, 11, errorThree, "#ffffff", "#0000aa");
+	render.text(startFour, 13, errorFour, "#ffffff", "#0000aa");
+}
 
 resumeThread = function() {
 	if (!thread.alive) {
@@ -241,16 +150,22 @@ resumeThread = function() {
 			for (var index in computer.eventStack[0]) {
 				var argument = computer.eventStack[0][index];
 				if (typeof(argument) == "string") {
-					C.lua_pushstring(thread.main, computer.eventStack[0][index]);
+					C.lua_pushstring(thread.main, argument);
 				} else if (typeof(argument) == "number") {
-					C.lua_pushnumber(thread.main, computer.eventStack[0][index]);
+					C.lua_pushnumber(thread.main, argument);
+				} else if (typeof(argument) == "boolean") {
+					C.lua_pushboolean(thread.main, argument ? 1 : 0);
+				} else if (typeof(argument) == "object") {
+					C.lua_pushstring(thread.main, filesystem.serializeTable(argument));
 				} else {
-					C.lua_pushstring(thread.main, computer.eventStack[0][index].toString());
+					C.lua_pushstring(thread.main, argument.toString());
 				}
 			}
 
 			computer.eventStack.splice(0, 1);
 
+			//C.lua_sethook(thread.main, tooLongHook, C.LUA_MASKCOUNT, 2^11);
+			numberOfRuns = 0;
 			var resp = C.lua_resume(thread.main, argumentsNumber);
 			if (resp == C.LUA_YIELD) {
 
@@ -272,7 +187,9 @@ resumeThread = function() {
 
 
 var initialization = function() {
-	termAPI.clear();
+	for (var i = 1; i <= term.height; i++) {
+		render.text(1, i, " ".repeat(term.width), "#ffffff", "#000000");
+	}
 
 	var resp = C.lua_resume(thread.main, 0);
 	if (resp != C.LUA_YIELD && resp != 0) {
@@ -284,22 +201,34 @@ var initialization = function() {
 		console.log("Thread closed");
 
 		for (var i = 1; i <= term.height; i++) {
-			render.text(1, i, " ".repeat(term.width), "#000000", "#0000aa");
+			render.text(1, i, " ".repeat(term.width), "#ffffff", "#0000aa");
 		}
 
 		var startPos = Math.round((term.width / 2) - ((7 + errorCode.length) / 2));
+		term.cursorBlink = false;
 		render.text(16, 7, "FATAL : BIOS ERROR", "#0000aa", "#ffffff");
 		render.text(startPos, 9, "ERROR: " + errorCode, "#ffffff", "#0000aa");
 
 		if (trace) {
 			console.log("Trace: " + trace);
-			render.text(9, 11, "-- SEE CONSOLE FOR MORE DETAILS --", "#ffffff", "#0000aa");
+			render.text(9, 11, "-- SEE CONSOLE FOR STACK TRACE --", "#ffffff", "#0000aa");
 		}
 	}
 }
 
 
-var main = function() {
+var setup = function(callback) {
+	filesystem.setup(function(err) {
+		if (err) {
+			return;
+		}
+
+		callback();
+	});
+}
+
+
+var run = function() {
 	loadAPIs();
 
 	setInterval(function() {
@@ -310,9 +239,15 @@ var main = function() {
 	startClock = Date.now();
 
 	thread.main = C.lua_newthread(L);
-	C.luaL_loadstring(thread.main, code);
+	C.luaL_loadstring(thread.main, getCode());
 	thread.alive = true;
 
-	setupFSAPI();
 	initialization();
+}
+
+
+var main = function() {
+	setup(function() {
+		run();
+	});
 };
