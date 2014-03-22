e23120369b7d10c3d2796447985d86b4bbeb77ed
Jason <1lanncontact@gmail.com>
2014-03-22 19:14:53 +0800

    more IE

--- scripts/render.js
+++ scripts/render.js
@@ -25,7 +25,6 @@ var lineYOffset = 2;
 
 
 render.setup = function(callback) {
-	context.imageSmoothingEnabled = false;
 	font = new Image();
 	font.src = "fonts/font.png";
 	font.onload = callback;
