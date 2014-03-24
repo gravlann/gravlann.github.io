958184c497d14b613c4ec19b49446e1e3b5282b3
Ben Anderson <gravityscore@gmail.com>
2014-03-24 19:05:14 +0800

    Updated

--- scripts/event.js
+++ scripts/event.js
@@ -6,11 +6,17 @@
 
 
 
-var mouseDown = false;
-var prevMouseState = {
-	"x": -1,
-	"y": -1,
-	"button": -1,
+var events = {
+	"prevMouseState": {
+		"x": -1,
+		"y": -1,
+		"button": -1,
+	},
+
+	"mouseDown": false,
+	"pasting": false,
+	"triggerKeyTimerID": null,
+	"triggerKey": null,
 };
 
 
@@ -26,6 +32,10 @@ window.onkeydown = function(event) {
 	}
 
 	var computer = core.getActiveComputer();
+	if (typeof(computer) == "undefined") {
+		return;
+	}
+
 	if (computer.hasErrored) {
 		if (event.keyCode == 13) {
 			computer.reboot();
@@ -33,38 +43,94 @@ window.onkeydown = function(event) {
 		}
 	}
 
-	var code = globals.keyCodes[event.keyCode];
+	var code = parseInt(globals.keyCodes[event.keyCode]);
 	var character = globals.characters.noshift[event.keyCode];
 	if (event.shiftKey) {
 		character = globals.characters.shift[event.keyCode];
 	}
 
-	console.log(event.keyCode);
+	if ((event.ctrlKey || event.metaKey) && character && character.toLowerCase() == "v") {
+		events.pasting = true;
 
-	var pushedSomething = false;
+		var captureField = $("#paste-capture");
+		captureField.focus();
 
-	if (typeof(code) != "undefined") {
-		computer.eventStack.push(["key", parseInt(code)]);
-		pushedSomething = true;
+		setTimeout(function() {
+			var pasted = captureField.val();
+			captureField.val("");
+
+			for (var i = 0; i < pasted.length; i++) {
+				var letter = pasted[i];
+				var keyCode = parseInt(globals.charCodes[letter]);
+				var code = globals.keyCodes[keyCode];
+
+				if (typeof(code) != "undefined") {
+					computer.eventStack.push(["key", code]);
+				}
+
+				if (typeof(letter) != "undefined") {
+					computer.eventStack.push(["char", letter]);
+				}
+			}
+			
+			if (pasted.length > 0) {
+				computer.resume();
+			}
+
+			events.pasting = false;
+		}, 10);
+	} else if (event.ctrlKey && character && character == "r" && !events.triggerKeyTimerID) {
+		events.triggerKeyTimerID = setTimeout(function() {
+			computer.reboot();
+			events.triggerKeyTimerID = null;
+		}, 1000);
+
+		events.triggerKey = "r";
+	} else if (event.ctrlKey && character && character == "s" && !events.triggerKeyTimerID) {
+		events.triggerKeyTimerID = setTimeout(function() {
+			computer.shutdown();
+			events.triggerKeyTimerID = null;
+		}, 1000);
+
+		events.triggerKey = "s";
+	} else if (event.ctrlKey && character && character == "t" && !events.triggerKeyTimerID) {
+		events.triggerKeyTimerID = setTimeout(function() {
+			computer.terminate();
+			events.triggerKeyTimerID = null;
+		}, 1000);
+
+		events.triggerKey = "t";
+	} else if (!events.triggerKeyTimerID) {
+		var pushedSomething = false;
+
+		if (typeof(code) != "undefined") {
+			computer.eventStack.push(["key", code]);
+			pushedSomething = true;
+		}
+
+		if (typeof(character) != "undefined") {
+			computer.eventStack.push(["char", character]);
+			pushedSomething = true;
+		}
+
+		if (pushedSomething) {
+			computer.resume();
+		}
 	}
 
-	if (typeof(character) != "undefined") {
-		computer.eventStack.push(["char", character]);
-		pushedSomething = true;
-	}
-
-	if (pushedSomething) {
-		computer.resume();
-	}
-
-	if (event.keyCode == 8) {
+	if (!events.pasting && (event.keyCode == 8 || event.keyCode == 86)) {
 		event.preventDefault();
 	}
 }
 
 
 window.onkeyup = function(event) {
+	var character = globals.characters.noshift[event.keyCode];
 
+	if (events.triggerKeyTimerID && character == events.triggerKey) {
+		clearTimeout(events.triggerKeyTimerID);
+		events.triggerKeyTimerID = null;
+	}
 }
 
 
@@ -79,7 +145,7 @@ window.onmousedown = function(event) {
 		return;
 	}
 
-	mouseDown = true;
+	events.mouseDown = true;
 
 	var computer = core.getActiveComputer();
 
@@ -95,7 +161,7 @@ window.onmousedown = function(event) {
 
 
 window.onmouseup = function(event) {
-	mouseDown = false;
+	events.mouseDown = false;
 }
 
 
@@ -111,14 +177,14 @@ window.onmousemove = function(event) {
 	var y = Math.floor((event.pageY - config.borderHeight - loc.y) / config.cellHeight) + 1;
 	var button = globals.buttons["click " + event.button];
 
-	if (mouseDown && (prevMouseState.button != button || prevMouseState.x != x || prevMouseState.y != y)) {
-		if (x >= 1 && y >= 1 && x <= computer.width && y <= computer.height) {
-			computer.eventStack.push(["mouse_drag", button, x, y]);
-			computer.resume();
+	if (events.mouseDown
+			&& (events.prevMouseState.button != button || events.prevMouseState.x != x || events.prevMouseState.y != y)
+			&& (x >= 1 && y >= 1 && x <= computer.width && y <= computer.height)) {
+		computer.eventStack.push(["mouse_drag", button, x, y]);
+		computer.resume();
 
-			prevMouseState.button = button;
-			prevMouseState.y = x;
-			prevMouseState.x = y;
-		}
+		events.prevMouseState.button = button;
+		events.prevMouseState.y = x;
+		events.prevMouseState.x = y;
 	}
 }
